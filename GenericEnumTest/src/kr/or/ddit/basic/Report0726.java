23a8c093bae16367675bb360bfe2733a9b31886f
yha2222 <yhaha5751@gmail.com>
2023-07-26 21:25:29 +0900

    정답..

--- GenericEnumTest/src/kr/or/ddit/basic/Report0726.java
+++ GenericEnumTest/src/kr/or/ddit/basic/Report0726.java
@@ -22,3 +22,46 @@ public class Report0726 {
 		
 	}
 }
+
+/*
+ * package enum_;
+
+public class EnumEx {
+	
+	enum Planet {
+		
+		수성(2439), 금성(6052), 지구(6371), 화성(3390), 
+		목성(69911), 토성(58232), 천왕성(25362), 해왕성(24622);
+		
+		private final int radius;
+		
+		private static final double PI = 3.1415926;
+		
+		private Planet(int radius) {
+			this.radius = radius;
+		}
+		
+		public int getRadius() {	
+			return radius;
+		}
+		
+		public double getArea() {
+			return 4*PI*radius*radius;
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (Planet planet : Planet.values()) {
+			System.out.println(planet.name() + "의 반지름 : " + planet.getRadius() + "Km\n면적 : " 
+							+ planet.getArea() + "Km²");
+			System.out.println("----------------------------------------");
+		}
+	}
+	
+	
+}
+
+
+
+
+ */
