543d772e0e11b4145b7634d6e8275142b944648a
yha2222 <yhaha5751@gmail.com>
2023-07-26 21:24:31 +0900

    enum

---
+++ GenericEnumTest/src/kr/or/ddit/basic/Report0726.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.basic;
+
+/*
+ 문제) 태양계 행성을 나타내는 enum Planet을 이용하여 구하시오.
+(단, enum 객체 생성시 반지름을 이용하도록 정의하시오.) 
+(태양계 행성의 반지름을 이용하여 면적구하기)
+예) 행성의 반지름(KM):
+수성(2439), 
+금성(6052), 
+지구(6371), 
+성(3390), 
+목성(69911), 
+토성(58232), 
+천왕성(25362), 
+해왕성(24622)
+ */
+public class Report0726 {
+	
+	//public enum 
+	
+	public static void main(String[] args) {
+		
+	}
+}
