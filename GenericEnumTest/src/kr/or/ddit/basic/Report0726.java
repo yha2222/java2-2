6a4a4ef074de8d00b27d64ae55641dfd225b8056
yha2222 <yhaha5751@gmail.com>
2023-07-27 01:33:45 +0900

    행성 면적 구하기 enum 사용

--- GenericEnumTest/src/kr/or/ddit/basic/Report0726.java
+++ GenericEnumTest/src/kr/or/ddit/basic/Report0726.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.basic;
 
+import kr.or.ddit.basic.T02EnumTest.City;
+
 /*
  문제) 태양계 행성을 나타내는 enum Planet을 이용하여 구하시오.
 (단, enum 객체 생성시 반지름을 이용하도록 정의하시오.) 
@@ -13,55 +15,37 @@ package kr.or.ddit.basic;
 토성(58232), 
 천왕성(25362), 
 해왕성(24622)
+
+π(파이)*r(반지름)^2
  */
 public class Report0726 {
 	
-	//public enum 
-	
-	public static void main(String[] args) {
+	public enum Planet {
+		수성(2439), 금성(6052), 지구(6371), 화성(3390), 목성(69911), 토성(58232), 천왕성(25362), 해왕성(24622);
 		
-	}
-}
-
-/*
- * package enum_;
-
-public class EnumEx {
-	
-	enum Planet {
-		
-		수성(2439), 금성(6052), 지구(6371), 화성(3390), 
-		목성(69911), 토성(58232), 천왕성(25362), 해왕성(24622);
-		
-		private final int radius;
+		private double KM;
 		
 		private static final double PI = 3.1415926;
 		
-		private Planet(int radius) {
-			this.radius = radius;
+		Planet(double km){
+			this.KM = km;
 		}
 		
-		public int getRadius() {	
-			return radius;
+		public double getKm() {
+			return KM;
 		}
 		
-		public double getArea() {
-			return 4*PI*radius*radius;
+		public double getR() {
+			double r = Math.pow(KM, 2);
+			return PI * r;
 		}
 	}
 	
 	public static void main(String[] args) {
-		for (Planet planet : Planet.values()) {
-			System.out.println(planet.name() + "의 반지름 : " + planet.getRadius() + "Km\n면적 : " 
-							+ planet.getArea() + "Km²");
-			System.out.println("----------------------------------------");
+		
+		for(Planet planet : Planet.values()) {
+			System.out.println(planet.name() + "의 반지름 : " + planet.getKm() + ", 면적: " + planet.getR());
 		}
 	}
-	
-	
 }
 
-
-
-
- */
